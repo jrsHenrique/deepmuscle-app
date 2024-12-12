5418d8bd4e8c794ebecac2757fbe4fd5dbb28293
SH3lby <jrs.henrique9@gmail.com>
2024-12-12 13:01:26 -0300

    initial test

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+export default {
+    transform: {
+      "^.+\\.(js|jsx|mjs)$": "babel-jest",
+    },
+  };
+  
---
+++ tests/ChatBox.test.jsx
@@ -0,0 +1,10 @@
+import {render, fireEvent, screen} from '@testing-library/react';
+import ChatBox from '../components/ChatBox';
+
+test('renders ChatBox and handles user input', ()=>{
+    render(<ChatBox/>);
+    const input = screen.getByPlaceholderText('Digite sua mensagem...');
+    fireEvent.change(input, {target: {value: 'Qual o melhor treino para hipertrofia?'}});
+    expect(input.value).toBe('Qual o melhor treino para hipertrofia?');
+}) 
+    
